1ca96ae4477fb92c7cbb479003d50ffbac39cbf8
vigneshwaran1409 <mvigneshwaran1409@gmail.com>
2024-07-11 10:27:43 +0530

    Url_Shortner_FE

---
+++ src/Components/footer.jsx
@@ -0,0 +1,22 @@
+
+const Footer = () =>{
+    return(
+        <>
+        {/* <!-- Footer -->*/}
+<footer className="text-center text-lg-start bg-light text-muted" style={{marginTop:"40vh"}}>
+  
+
+  {/* <!-- Copyright -->*/}
+  <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
+    © 2024 Copyright : 
+    <br/>
+    <a className="text-reset fw-bold" href="">Vigneshwaran(URL Shortner)</a>
+  </div>
+  {/* <!-- Copyright -->*/}
+</footer>
+{/* <!-- Footer -->*/}
+        </>
+    )
+}
+
+export default Footer
